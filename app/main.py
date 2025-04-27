7c92afc3335966a5ad7b67e0371ac5af5acecc6b
greysnell <greysnell20@gmail.com>
2025-04-27 12:52:29 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,9 +12,11 @@ def create_person_list(people: list[dict]) -> list:
     person_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in person_list:
-        person_data = next(p for p in people if p["name"] == person.name)
-        spouse = person_data.get("wife") or person_data.get("husband")
-        if spouse:
-            setattr(person, "wife" if "wife" in person_data else "husband", Person.people[spouse])
-
-    return person_list
+    person_data = next(p for p in people if p["name"] == person.name)
+    spouse = person_data.get("wife") or person_data.get("husband")
+    if spouse:
+        setattr(
+            person,
+            "wife" if "wife" in person_data else "husband",
+            Person.people[spouse]
+        )
